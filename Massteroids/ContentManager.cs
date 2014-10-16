e2cf52bb16f40d1430142b6ee481adc716a46a82
Will Hamilton <whamilton42@hotmail.com>
2014-10-16 22:23:03 +0100

    Initial commit
    Custom content manager

---
+++ Massteroids/ContentManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Massteroids
+{
+    class ContentManager
+    {
+        String baseLocation;
+        GraphicsDeviceManager graphics;
+
+        public ContentManager(GraphicsDeviceManager graphics)
+        {
+            this.graphics = graphics;
+            this.baseLocation = System.IO.Directory.GetCurrentDirectory() + "\\..\\..\\Content\\";
+        }
+
+        public Texture2D load(String fileName)
+        {
+            String fileLocation = baseLocation + fileName;
+            System.IO.Stream stream = new System.IO.FileStream(fileLocation, System.IO.FileMode.Open);
+
+            return Texture2D.FromStream(graphics.GraphicsDevice, stream);
+        }
+    }
+}
---
+++ Massteroids/Game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Massteroids
+{
+    class Game : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D texture;
+        ContentManager contentManager;
+
+        public Game(){
+            graphics = new GraphicsDeviceManager(this);
+            contentManager = new ContentManager(graphics);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            texture = contentManager.load("shuttle.png");
+        }
+
+        protected override void UnloadContent()
+        {
+            Content.Unload();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, new Vector2(0, 0), Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
